effa13b8e697ed281e995f5c28507d71fc376a08
Raevsk1i <raevski_yt@mail.ru>
2026-02-02 15:18:43 +0300

    init app

---
+++ src/main/java/com/github/raevsk1i/dt2influx/Dt2influxApplication.java
@@ -0,0 +1,13 @@
+package com.github.raevsk1i.dt2influx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dt2influxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dt2influxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/raevsk1i/dt2influx/Dt2influxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.raevsk1i.dt2influx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dt2influxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
