e197efe4ae98008bc8127a3cf2953b36eb6b07eb
Raevsk1i <raevski_yt@mail.ru>
2026-02-05 14:17:53 +0300

    made CreateJobRequestDto params

--- src/main/java/com/github/raevsk1i/dt2influx/dto/request/CreateJobRequestDto.java
+++ src/main/java/com/github/raevsk1i/dt2influx/dto/request/CreateJobRequestDto.java
@@ -1,4 +1,15 @@
 package com.github.raevsk1i.dt2influx.dto.request;
 
+
+import lombok.NonNull;
+import lombok.Data;
+
+@Data
 public class CreateJobRequestDto {
+
+    @NonNull
+    private String namespace;
+
+    @NonNull
+    private String mzId;
 }
