7b7d2c4fbe34b8ab794c5a4981bca09c69c80d75
Raevsk1i <raevski_yt@mail.ru>
2026-02-05 14:19:13 +0300

    reworked IJobScheduler

--- src/main/java/com/github/raevsk1i/dt2influx/service/IJobScheduler.java
+++ src/main/java/com/github/raevsk1i/dt2influx/service/IJobScheduler.java
@@ -3,13 +3,13 @@ package com.github.raevsk1i.dt2influx.service;
 import com.github.raevsk1i.dt2influx.entity.JobInfo;
 import com.github.raevsk1i.dt2influx.job.AbstractJob;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 public interface IJobScheduler {
+    ConcurrentHashMap<String, ScheduledFuture<?>> map = new ConcurrentHashMap<>();
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(0);
 
-    JobInfo scheduleJob(AbstractJob job);
+    JobInfo scheduleJob(AbstractJob job, Integer interval);
 
-    JobInfo stopScheduledJob(String namespace);
+    JobInfo stopScheduledJob(String namespace) throws InterruptedException;
 }
