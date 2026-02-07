6f2bb2c58458f9561745ff7db3cb6eb163017b43
Raevsk1i <raevski_yt@mail.ru>
2026-02-07 19:33:55 +0300

    made params for JobsAliveResponseDto

--- src/main/java/com/github/raevsk1i/dt2influx/dto/response/JobsAliveResponseDto.java
+++ src/main/java/com/github/raevsk1i/dt2influx/dto/response/JobsAliveResponseDto.java
@@ -1,4 +1,11 @@
 package com.github.raevsk1i.dt2influx.dto.response;
 
+import com.github.raevsk1i.dt2influx.entity.JobInfo;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class JobsAliveResponseDto {
+    List<JobInfo> jobs;
 }
