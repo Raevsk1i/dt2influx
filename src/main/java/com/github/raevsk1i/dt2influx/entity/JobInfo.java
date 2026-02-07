855ae983ea4afe32387e00f263945f8237b98ace
Raevsk1i <raevski_yt@mail.ru>
2026-02-07 19:32:34 +0300

    reworked JobInfo

--- src/main/java/com/github/raevsk1i/dt2influx/entity/JobInfo.java
+++ src/main/java/com/github/raevsk1i/dt2influx/entity/JobInfo.java
@@ -1,12 +1,28 @@
 package com.github.raevsk1i.dt2influx.entity;
 
+import com.github.raevsk1i.dt2influx.enums.JobType;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class JobInfo {
+
+    public JobInfo(String namespace, String mzId, JobType type) {
+        this.namespace = namespace;
+        this.mzId = mzId;
+        this.type = type;
+    }
+
     private String namespace;
+    private String mzId;
+    private JobType type;
     private LocalDateTime createDate;
     private String name;
+    private boolean cancelled = false;
+
+    public JobInfo cancel() {
+        cancelled = true;
+        return this;
+    }
 }
