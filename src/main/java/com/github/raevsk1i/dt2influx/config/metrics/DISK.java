e768ab169cc6ef88c17ccd668059840bb1277ecc
Raevsk1i <raevski_yt@mail.ru>
2026-02-09 17:04:06 +0300

    Created Disk metric class

--- src/main/java/com/github/raevsk1i/dt2influx/config/metrics/DISK.java
+++ src/main/java/com/github/raevsk1i/dt2influx/config/metrics/DISK.java
@@ -1,4 +1,35 @@
 package com.github.raevsk1i.dt2influx.config.metrics;
 
+import com.github.raevsk1i.dt2influx.entity.MetricDefinition;
+
+import java.util.List;
+
 public final class DISK {
+    public static final List<MetricDefinition> metrics = List.of(
+            new MetricDefinition(
+                    "avail_bytes",
+                    "(builtin:host.disk.avail:splitBy(\"dt.entity.host\"):sum):names"),
+            new MetricDefinition(
+                    "used_bytes",
+                    "(builtin:host.disk.used:splitBy(\"dt.entity.host\"):sum):names"),
+            new MetricDefinition(
+                    "read_ops",
+                    "(builtin:host.disk.read0ps:splitBy(\"dt.entity.host\"):sum):names"),
+            new MetricDefinition(
+                    "write_ops",
+                    "(builtin:host.disk.writeOps:splitBy(\"dt.entity.host\"):sum):names"),
+            new MetricDefinition(
+                    "read_bytes",
+                    "(builtin:host.disk.bytesRead:splitBy(\"dt.entity.host\")):sum:names"),
+            new MetricDefinition(
+                    "written_bytes",
+                    "(builtin:host.disk.bytesWritten:splitBy(\"dt.entity.host\")):sum:names")
+
+
+//          TODO: if new metrics will available
+
+//            new MetricDefinition(
+//                    "",
+//                    "")
+    );
 }
