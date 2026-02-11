6fe9f0b4b055d86660832b4869b94c4b79778226
Raevsk1i <raevski_yt@mail.ru>
2026-02-11 18:00:15 +0300

    updated AbstractJob

--- src/main/java/com/github/raevsk1i/dt2influx/job/AbstractJob.java
+++ src/main/java/com/github/raevsk1i/dt2influx/job/AbstractJob.java
@@ -4,16 +4,48 @@ import com.github.raevsk1i.dt2influx.entity.JobInfo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Map;
+
 @Getter
 @Setter
 public abstract class AbstractJob implements Runnable {
 
     private JobInfo info;
 
-    public AbstractJob(JobInfo info) {
+    private String reflexUrl;
+    private String token;
+
+    public AbstractJob(JobInfo info, String reflexUrl, String token) {
         this.info = info;
+        this.reflexUrl = reflexUrl;
     }
 
     @Override
     public abstract void run();
+
+    protected String encodeParams(Map<String, String> params, String metricSelector, String reflexUrl) {
+
+        StringBuilder paramsBuilder = new StringBuilder();
+
+        paramsBuilder
+                .append("metricSelector")
+                .append("=")
+                .append(URLEncoder.encode(metricSelector, Charset.defaultCharset()))
+                .append("&");
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            paramsBuilder
+                    .append(entry.getKey())
+                    .append("=")
+                    .append(URLEncoder.encode(entry.getValue(), Charset.defaultCharset()));
+
+            if (!entry.equals(params.entrySet().toArray()[params.size() - 1])) {
+                paramsBuilder.append("&");
+            }
+        }
+
+        return reflexUrl + paramsBuilder;
+    }
 }
