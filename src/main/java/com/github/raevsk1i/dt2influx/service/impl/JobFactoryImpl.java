839608f17100c2520df08a9d468d6abf7f3c54e9
Raevsk1i <raevski_yt@mail.ru>
2026-02-11 18:00:52 +0300

    reworked impl for JobFactory

--- src/main/java/com/github/raevsk1i/dt2influx/service/impl/JobFactoryImpl.java
+++ src/main/java/com/github/raevsk1i/dt2influx/service/impl/JobFactoryImpl.java
@@ -1,11 +1,13 @@
 package com.github.raevsk1i.dt2influx.service.impl;
 
+import com.github.raevsk1i.dt2influx.config.ReflexConfig;
 import com.github.raevsk1i.dt2influx.entity.JobInfo;
 import com.github.raevsk1i.dt2influx.enums.JobType;
 import com.github.raevsk1i.dt2influx.exceptions.NoSuitableJobException;
 import com.github.raevsk1i.dt2influx.job.AbstractJob;
 import com.github.raevsk1i.dt2influx.job.FPJob;
 import com.github.raevsk1i.dt2influx.service.IJobFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -13,12 +15,19 @@ import java.time.LocalDateTime;
 @Component
 public class JobFactoryImpl implements IJobFactory {
 
+    private final ReflexConfig reflexConfig;
+
+    @Autowired
+    public JobFactoryImpl(ReflexConfig reflexConfig) {
+        this.reflexConfig = reflexConfig;
+    }
+
     @Override
     public AbstractJob createJob(JobInfo info) {
         info.setCreateDate(LocalDateTime.now());
         switch (info.getType()) {
             case JobType.FP, JobType.ONETIME -> {
-                return new FPJob(info);
+                return new FPJob(info, reflexConfig.getUrl(), reflexConfig.getToken());
             }
             case JobType.DB -> {
                 return null;
