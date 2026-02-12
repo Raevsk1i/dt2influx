1f0b0c5bbf0e51da17d494cd63a3a340b2ac3adc
Raevsk1i <raevski_yt@mail.ru>
2026-02-12 14:28:59 +0300

    created HttpCleintIsNullException

---
+++ src/main/java/com/github/raevsk1i/dt2influx/exceptions/HttpClientIsNullException.java
@@ -0,0 +1,7 @@
+package com.github.raevsk1i.dt2influx.exceptions;
+
+public class HttpClientIsNullException extends RuntimeException {
+    public HttpClientIsNullException(String message) {
+        super(message);
+    }
+}
