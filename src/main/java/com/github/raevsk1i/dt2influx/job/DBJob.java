62aac53fcba5142e4f39fe47451f12e9fe86f35c
Raevsk1i <raevski_yt@mail.ru>
2026-02-12 15:43:42 +0300

    little changes for not realized DBJob

--- src/main/java/com/github/raevsk1i/dt2influx/job/DBJob.java
+++ src/main/java/com/github/raevsk1i/dt2influx/job/DBJob.java
@@ -4,8 +4,8 @@ import com.github.raevsk1i.dt2influx.entity.JobInfo;
 
 public class DBJob extends AbstractJob{
 
-    public DBJob(JobInfo info) {
-        super(info);
+    public DBJob(JobInfo info, String reflexUrl, String token) {
+        super(info, reflexUrl, token);
     }
 
     @Override
