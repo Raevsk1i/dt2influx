c219ced70fae1991d1821dc8fd3eddf6873deb5d
Raevsk1i <raevski_yt@mail.ru>
2026-02-12 15:38:24 +0300

    new exception for if reflex http response is null

---
+++ src/main/java/com/github/raevsk1i/dt2influx/exceptions/ReflexResponseIsNotValidException.java
@@ -0,0 +1,7 @@
+package com.github.raevsk1i.dt2influx.exceptions;
+
+public class ReflexResponseIsNotValidException extends RuntimeException {
+    public ReflexResponseIsNotValidException(String message) {
+        super(message);
+    }
+}
