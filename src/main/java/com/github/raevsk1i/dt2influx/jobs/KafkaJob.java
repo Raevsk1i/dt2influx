9bb29f20e3e3c8d5350384aebfcfd8614074f9ce
Raevsk1i <raevski_yt@mail.ru>
2026-02-20 16:49:24 +0300

    A rudimentary Kafka job has been created

--- src/main/java/com/github/raevsk1i/dt2influx/jobs/KafkaJob.java
+++ src/main/java/com/github/raevsk1i/dt2influx/jobs/KafkaJob.java
@@ -1,4 +1,16 @@
 package com.github.raevsk1i.dt2influx.jobs;
 
-public class KafkaJob {
+import com.github.raevsk1i.dt2influx.config.ReflexConfig;
+import com.github.raevsk1i.dt2influx.entity.JobInfo;
+
+public class KafkaJob extends AbstractJob {
+
+    public KafkaJob(JobInfo info, ReflexConfig reflexConfig) {
+        super(info, reflexConfig);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Starting Kafka Job");
+    }
 }
