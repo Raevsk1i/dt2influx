6c7f4bbfdf07b33f6be7368013e23405d63ff2cd
Raevsk1i <raevski_yt@mail.ru>
2026-02-26 16:47:41 +0300

    Minor changes: DBMetrics are disabled and IJob is implemented for KafkaJob

--- src/main/java/com/github/raevsk1i/dt2influx/jobs/KafkaJob.java
+++ src/main/java/com/github/raevsk1i/dt2influx/jobs/KafkaJob.java
@@ -3,7 +3,7 @@ package com.github.raevsk1i.dt2influx.jobs;
 import com.github.raevsk1i.dt2influx.config.ReflexConfig;
 import com.github.raevsk1i.dt2influx.entity.JobInfo;
 
-public class KafkaJob extends AbstractJob {
+public class KafkaJob extends AbstractJob implements IJob {
 
     public KafkaJob(JobInfo info, ReflexConfig reflexConfig) {
         super(info, reflexConfig);
