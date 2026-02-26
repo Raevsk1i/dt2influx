aef4de516db8b5289faf697f6c324b0bafe49244
Raevsk1i <raevski_yt@mail.ru>
2026-02-26 17:41:22 +0300

    The loadDatabases method has been implemented, and the loadDatabases method has been left empty to allow for overloading and implementing other logic for pulling database data.

--- src/main/java/com/github/raevsk1i/dt2influx/service/DatabaseStorage.java
+++ src/main/java/com/github/raevsk1i/dt2influx/service/DatabaseStorage.java
@@ -1,11 +1,20 @@
 package com.github.raevsk1i.dt2influx.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.raevsk1i.dt2influx.entity.DatabaseInfo;
+import com.github.raevsk1i.dt2influx.utils.HttpsUtils;
 import jakarta.annotation.PostConstruct;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +31,7 @@ public class DatabaseStorage {
 
     @PostConstruct
     public void init() {
-        loadDatabases();
+        loadDatabases(loader);
     }
 
     public void addDatabase(DatabaseInfo databaseInfo) {
@@ -39,9 +48,51 @@ public class DatabaseStorage {
         return dbs;
     }
 
+    private void loadDatabases(String loader) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            HttpClient client = HttpsUtils.getHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(loader)).build();
+
+            log.info("Request to load databases from {}", request.toString());
+
+            if (client == null || client.isTerminated()) {
+                log.warn("client is null or terminated");
+                return;
+            }
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if  (response.statusCode() != 200) {
+                log.warn("Failed load databases from {}", request.toString());
+                log.warn(response.body());
+                return;
+            }
+
+            DatabaseLoaderInfoDto loaderInfoDto = mapper.convertValue(response.body(), DatabaseLoaderInfoDto.class);
+
+            loaderInfoDto.getDbs().stream().parallel().forEach(db -> databases.put(db.getHost(), db));
+            client.close();
+            log.info("Database loader successfully pushed and has been closed");
+
+        } catch (ConnectException ex) {
+            log.error("Loader uri is invalid", ex);
+        } catch (IOException | InterruptedException ex) {
+            log.error(ex.getMessage(), ex);
+        }
+    }
+
     private void loadDatabases() {
         /*
-        TODO: реализовать логику загрузки баз данных из базы данных)
+        TODO: реализовать логику загрузки баз данных из базы данных, тем самым перегрузив метод
          */
     }
+
+
+}
+
+@Data
+class DatabaseLoaderInfoDto {
+    private List<DatabaseInfo> dbs;
 }
